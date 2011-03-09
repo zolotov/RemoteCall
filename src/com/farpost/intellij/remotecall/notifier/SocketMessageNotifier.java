96cb97069f140d7eeaddf03e3e61c5f6e26c0d85
Alexander Zolotov <zolotov@farpost.com>
2011-03-09 12:08:05 +1000

    Change format of request to HTTP-request. Add permissions for remote url

--- src/com/farpost/intellij/remotecall/notifier/SocketMessageNotifier.java
+++ src/com/farpost/intellij/remotecall/notifier/SocketMessageNotifier.java
@@ -8,14 +8,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.*;
 
 public class SocketMessageNotifier implements MessageNotifier {
 
 	private final Logger log = Logger.getInstance(getClass().getName());
 	private Collection<MessageHandler> messageHandlers = new HashSet<MessageHandler>();
 	private ServerSocket serverSocket;
+	private static final String CRLF = "\r\n";
 
 	public SocketMessageNotifier(ServerSocket serverSocket) {
 		this.serverSocket = serverSocket;
@@ -30,28 +30,59 @@ public class SocketMessageNotifier implements MessageNotifier {
 			Socket clientSocket;
 			try {
 				clientSocket = serverSocket.accept();
+				if (!clientSocket.getInetAddress().getHostAddress().equals("127.0.0.1")) {
+					log.warn("Not allowed requests from " + clientSocket.getInetAddress().getHostAddress());
+					continue;
+				}
+
 			} catch (IOException e) {
 				log.error("Error while accepting", e);
 				continue;
 			}
+
 			try {
 				BufferedReader in = new BufferedReader(
 					new InputStreamReader(clientSocket.getInputStream())
 				);
 
-				String inputLine, message = "";
+				String inputLine, requestString = "";
 
-
-				while ((inputLine = in.readLine()) != null) {
-					message += inputLine;
+				while ((inputLine = in.readLine()) != null && !inputLine.equals(CRLF) && !inputLine.equals("")) {
+					requestString += inputLine;
 				}
 
+				StringTokenizer tokenizer = new StringTokenizer(requestString);
+				String method = tokenizer.nextToken();
+				if (!method.equals("GET")) {
+					log.warn("Only GET requests allowed");
+					continue;
+				}
+
+				log.info("Received request " + requestString);
+				Map<String, String> parameters = getParametersFromUrl(tokenizer.nextToken());
+
+				String message = parameters.get("message") != null ? parameters.get("message") : "";
+
 				log.info("Received message " + message);
-				handleMessage(message);
+				handleMessage(requestString);
 			} catch (IOException e) {
 				log.error("Error", e);
 			}
 		}
+	}
+
+	private Map<String, String> getParametersFromUrl(String url) {
+		String parametersString = url.substring(url.indexOf('?') + 1);
+		Map<String, String> parameters = new HashMap<String, String>();
+		StringTokenizer tokenizer = new StringTokenizer(parametersString, "&");
+		while (tokenizer.hasMoreElements()) {
+			String[] parametersPair = tokenizer.nextToken().split("=", 2);
+			if (parametersPair.length > 1) {
+				parameters.put(parametersPair[0], parametersPair[1]);
+			}
+		}
+
+		return parameters;
 	}
 
 
