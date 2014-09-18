17f168fc5c3ed023002032f3fd12bc37c03412c5
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2014-09-18 15:32:23 +0400

    Get rid of guava dependecy

--- src/main/java/com/farpost/intellij/remotecall/RemoteCallComponent.java
+++ src/main/java/com/farpost/intellij/remotecall/RemoteCallComponent.java
@@ -16,46 +16,46 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 
 public class RemoteCallComponent implements ApplicationComponent {
-  private static final Logger log = Logger.getInstance(RemoteCallComponent.class);
-  
-	private ServerSocket serverSocket;
-	private Thread listenerThread;
+    private static final Logger log = Logger.getInstance(RemoteCallComponent.class);
 
-  public void initComponent() {
-    final int port = SystemProperties.getIntProperty("idea.remote.call.port", 8091);
-		try {
-			serverSocket = new ServerSocket();
-      serverSocket.bind(new InetSocketAddress("localhost", port));
-			log.info("Listening " + port);
-    } catch (IOException e) {
-			ApplicationManager.getApplication().invokeLater(new Runnable() {
-				public void run() {
-					Messages.showMessageDialog("Can't bind with " + port + " port. RemoteCall plugin won't work",
-						"RemoteCall Plugin Error", Messages.getErrorIcon());
-				}
-			});
-			return;
-		}
+    private ServerSocket serverSocket;
+    private Thread listenerThread;
 
-		MessageNotifier messageNotifier = new SocketMessageNotifier(serverSocket);
-		messageNotifier.addMessageHandler(new OpenFileMessageHandler(new FileNavigatorImpl()));
-		listenerThread = new Thread(messageNotifier);
-		listenerThread.start();
-	}
+    public void initComponent() {
+        final int port = SystemProperties.getIntProperty("idea.remote.call.port", 8091);
+        try {
+            serverSocket = new ServerSocket();
+            serverSocket.bind(new InetSocketAddress("localhost", port));
+            log.info("Listening " + port);
+        } catch (IOException e) {
+            ApplicationManager.getApplication().invokeLater(new Runnable() {
+                public void run() {
+                    Messages.showMessageDialog("Can't bind with " + port + " port. RemoteCall plugin won't work",
+                            "RemoteCall Plugin Error", Messages.getErrorIcon());
+                }
+            });
+            return;
+        }
 
-	public void disposeComponent() {
-		try {
-			if (listenerThread != null) {
-				listenerThread.interrupt();
-			}
-			serverSocket.close();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+        MessageNotifier messageNotifier = new SocketMessageNotifier(serverSocket);
+        messageNotifier.addMessageHandler(new OpenFileMessageHandler(new FileNavigatorImpl()));
+        listenerThread = new Thread(messageNotifier);
+        listenerThread.start();
+    }
 
-	@NotNull
-	public String getComponentName() {
-		return "RemoteCallComponent";
-	}
+    public void disposeComponent() {
+        try {
+            if (listenerThread != null) {
+                listenerThread.interrupt();
+            }
+            serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @NotNull
+    public String getComponentName() {
+        return "RemoteCallComponent";
+    }
 }
