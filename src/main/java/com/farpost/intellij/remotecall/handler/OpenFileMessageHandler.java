189feb813b997c449f4636c24e583b627602546f
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2014-09-23 01:58:52 +0400

    Get rid of yellow code

--- src/main/java/com/farpost/intellij/remotecall/handler/OpenFileMessageHandler.java
+++ src/main/java/com/farpost/intellij/remotecall/handler/OpenFileMessageHandler.java
@@ -1,17 +1,14 @@
 package com.farpost.intellij.remotecall.handler;
 
 import com.farpost.intellij.remotecall.utils.FileNavigator;
-import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.util.text.StringUtil;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static java.lang.Integer.parseInt;
 import static java.util.regex.Pattern.compile;
 
 public class OpenFileMessageHandler implements MessageHandler {
-
-  private static final Logger log = Logger.getInstance(OpenFileMessageHandler.class);
   private static final Pattern COLUMN_PATTERN = compile("[:#](\\d+)[:#]?(\\d*)$");
   private final FileNavigator fileNavigator;
 
@@ -26,14 +23,11 @@ public class OpenFileMessageHandler implements MessageHandler {
     int column = 0;
 
     if (matcher.find()) {
-      try {
-        line = parseInt(matcher.group(1)) - 1;
-        if (!matcher.group(2).isEmpty()) {
-          column = parseInt(matcher.group(2)) - 1;
-        }
-      }
-      catch (NumberFormatException e) {
-        log.error("Impossible situation, but who knows... RemoteCall extracting line/column number error", e);
+
+      line = StringUtil.parseInt(StringUtil.notNullize(matcher.group(1)), 1) - 1;
+      final String columnNumberString = matcher.group(2);
+      if (StringUtil.isNotEmpty(columnNumberString)) {
+        column = StringUtil.parseInt(columnNumberString, 1) - 1;
       }
     }
 
